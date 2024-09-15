55194ed99f0f6652543c119994e0bf9d1ddb49b0
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2024-09-15 09:25:30 +0530

    Time: 3 ms (40.74%), Space: 7.8 MB (58.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     bool isValid(string s) {
-    
-    std::stack<char> stk;
-        
+        std::stack<char> stk;
         for (char ch : s) {
+
             switch (ch) {
-                case '(':
-                case '{':
-                case '[':
-                    stk.push(ch);
-                    break;
-                case ')':
-                    if (stk.empty() || stk.top() != '(') return false;
+            case '(':
+            case '{':
+            case '[':
+                stk.push(ch);
+                break;
+            case ')':
+                if (stk.empty()||stk.top() != '(' ) {
+                    return false;
+                } else {
                     stk.pop();
                     break;
-                case '}':
-                    if (stk.empty() || stk.top() != '{') return false;
+                }
+            case '}':
+                if (stk.empty()||stk.top() != '{') {
+                    return false;
+                } else {
                     stk.pop();
                     break;
-                case ']':
-                    if (stk.empty() || stk.top() != '[') return false;
+                }
+            case ']':
+                if (stk.empty()||stk.top() != '[' ) {
+                    return false;
+                } else {
                     stk.pop();
                     break;
-                default:
-                    return false; // Invalid character
+                }
+            default:
+                return false;
             }
         }
-        
-        return stk.empty(); 
+        return stk.empty();
     }
 };
