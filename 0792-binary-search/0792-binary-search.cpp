f4bfd51748a342c4e7d2efe6d0a8bf2ffe64ee37
Jhalak Bhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-07-24 21:26:37 +0530

    Time: 0 ms (100%), Space: 31.2 MB (79.89%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i =0;
+        while(i != nums.size()){
+            if(nums[i]==target){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+};
