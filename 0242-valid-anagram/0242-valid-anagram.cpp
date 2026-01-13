3521e5b32569eb902bd719e5972d5517f79df78c
Jhalak Bhandari <82494331+jhalakbhandari@users.noreply.github.com>
2026-01-13 12:57:22 +0530

    Time: 2 ms (51.8%), Space: 10 MB (10.84%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       unordered_map<char,int>mp1,mp2;
+       for(char ch:s){
+        mp1[ch]++;
+       }
+       for(char ch:t){
+        mp2[ch]++;
+       }
+       if(mp1==mp2)
+            return true;
+       return false;
+    }
+};
